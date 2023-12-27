cf59f3eafabb6661ad42ca6c5747c41f5c75756e
sevval29 <78108213+sevval29@users.noreply.github.com>
2023-12-27 03:34:10 +0300

    Jwt token ile geliştirme yapıldı

---
+++ JwtWebApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApi.Models
+{
+    public class User
+    {
+
+       public string Username { get; set; }   =string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}
---
+++ JwtWebApi/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace JwtWebApi.Models
+{
+    public class UserDto
+    {
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
